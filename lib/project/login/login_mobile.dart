bb8d523f7d40e23c5c021a834a107f8763e542e4
PrasadMusini-Dev <durgaprasad.musini@calibrage.in>
2024-07-04 18:04:31 +0530

    commiting

---
+++ lib/project/login/login_mobile.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:practice_flutter/temp/page2.dart';
+
+class LoginMobile extends StatefulWidget {
+  const LoginMobile({super.key});
+
+  @override
+  State<LoginMobile> createState() => _LoginMobileState();
+}
+
+class _LoginMobileState extends State<LoginMobile> {
+  Color textFieldFillColor = const Color(0xFFfafafa);
+  Color blueColor = const Color(0xFF7ba4d7);
+  bool? checkValue = false;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: signinFromMobile(),
+    );
+  }
+
+  Widget signinFromMobile() {
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30)
+              .copyWith(top: size.height / 5),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Welcome back'),
+                const Text('Please enter your informations'),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CustomField(
+                  labelText: 'Name',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CustomField(
+                  labelText: 'Email',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const CustomField(
+                  labelText: 'Password',
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Checkbox(
+                          value: checkValue,
+                          shape: const CircleBorder(),
+                          side: const BorderSide(
+                              strokeAlign: BorderSide.strokeAlignCenter),
+                          onChanged: (value) {
+                            setState(() {
+                              checkValue = value;
+                            });
+                          },
+                        ),
+                        const Text('Remember me'),
+                      ],
+                    ),
+                    const Text('Forgot Password'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomBtn(
+                        backgroundColor: Colors.black,
+                        radius: 5,
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => const Home(),
+                            ),
+                          );
+                        },
+                        child: const Text('Login'),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: horizontalDivider(),
+                    ),
+                    const Text('   OR   '),
+                    Expanded(
+                      child: horizontalDivider(),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    authBox(Icons.golf_course),
+                    authBox(Icons.egg_alt_rounded),
+                    authBox(Icons.unarchive_rounded),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account? '),
+                    Text('Sign up for free :)'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget authBox(IconData icon) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        backgroundColor: textFieldFillColor,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        side: BorderSide(color: Colors.grey.shade400),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: Icon(icon),
+    );
+  }
+
+  Widget horizontalDivider() {
+    return Container(
+      height: 0.5,
+      color: Colors.grey.shade400,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
---
+++ lib/project/login/login_web.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:practice_flutter/project/login/login_mobile.dart';
+
+class LoginWeb extends StatelessWidget {
+  const LoginWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: size.width * 0.07,
+            vertical: size.height * 0.05,
+          ),
+          color: Colors.grey,
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  // width: size.width * 0.5 - size.width * 0.07,
+                  // height: size.height * 0.8 - size.height * 0.05,
+                  color: Colors.grey[200],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  // width: size.width * 0.5 - size.width * 0.07,
+                  // height: size.height * 0.8 - size.height * 0.05,
+                  color: Colors.grey[300],
+                ),
+              ),
+            ],
+          )
+
+          /*  Row(
+          children: [
+            Container(
+              width: size.width * 0.5 - size.width * 0.07,
+              height: size.height * 0.8 - size.height * 0.05,
+              color: Colors.red[200],
+            ),
+            // Container(
+            //   width: size.width * 0.5 - size.width * 0.07,
+            //   height: size.height * 0.8 - size.height * 0.05,
+            //   color: Colors.white,
+            //   padding: EdgeInsets.symmetric(
+            //     horizontal: size.width * 0.07,
+            //   ),
+            //   child: const LoginMobile(),
+            // ),
+          ],
+        ), */
+          ),
+    );
+  }
+
+  Widget box({required Color color, int quarterTurns = 0, double size = 100}) {
+    return RotatedBox(
+      quarterTurns: quarterTurns,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color,
+        ),
+      ),
+    );
+  }
+}
