1ffffc7f3f7f1ec56f7ab6fadc33c42837bb430d
PrasadMusini-Dev <durgaprasad.musini@calibrage.in>
2024-07-10 18:42:13 +0530

    Made changes in main method and revoked

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:upi_payment_qrcode_generator/upi_payment_qrcode_generator.dart';
+import 'package:practice_flutter/project/navigation/router_config.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-/// Creates The UPI Payment QRCode
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final upiDetailsWithAmount = UPIDetails(
-    upiID: "prasadmusini1999@axl",
-    payeeName: "with amount",
-    amount: 1,
-    transactionNote: "One Rupee",
-  );
-  final upiDetailsWithoutAmount = UPIDetails(
-    upiID: "prasadmusini1999@axl",
-    payeeName: "without amount",
-    transactionNote: "No amount specified",
-  );
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('UPI Payment QRCode Generator'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // const Text("UPI Payment QRCode without Amount",
-              //     style: TextStyle(fontWeight: FontWeight.bold)),
-              // UPIPaymentQRCode(
-              //   upiDetails: upiDetailsWithoutAmount,
-              //   size: 220,
-              //   embeddedImagePath: Assets.images.googleLogo.path,
-              //   embeddedImageSize: const Size(60, 60),
-              //   eyeStyle: const QrEyeStyle(
-              //     eyeShape: QrEyeShape.circle,
-              //     color: Colors.black,
-              //   ),
-              //   dataModuleStyle: const QrDataModuleStyle(
-              //     dataModuleShape: QrDataModuleShape.circle,
-              //     color: Colors.black,
-              //   ),
-              // ),
-              const SizedBox(height: 30),
-              Text(
-                "Scan QR to Pay",
-                style: TextStyle(color: Colors.grey[600], letterSpacing: 1.2),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text("UPI Payment QRCode with Amount",
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              UPIPaymentQRCode(
-                upiDetails: upiDetailsWithAmount,
-                size: 220,
-                upiQRErrorCorrectLevel: UPIQRErrorCorrectLevel.high,
-              ),
-              Text(
-                "Scan QR to Pay",
-                style: TextStyle(color: Colors.grey[600], letterSpacing: 1.2),
-              ),
-            ],
-          ),
-        ),
-      ),
+    // return const MaterialApp(
+    //   home: ProfileMobile(),
+    // );
+    return MaterialApp.router(
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
